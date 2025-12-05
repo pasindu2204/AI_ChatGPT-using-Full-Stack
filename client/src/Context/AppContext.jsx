763fe4157f9746ca1021ae3e962128d2721ebe69
Pasindu <chamipasindu2204@gmail.com>
2025-12-05 17:44:25 +0530

    add variables and functions in appcontext page

--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -1,11 +1,62 @@
-import { createContext, use, useContext } from 'react'
+import { createContext, useState, useContext, useEffect, use } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 const AppContext = createContext()
 
 export const AppContextProvider = ({children}) => {
 
-    const value = {}
+    const navigate = useNavigate()
+    const [user, setUser] = useState(null);
+    const [chats, setChats] = useState([]);
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    const fetchUser = async (params) => {
+        setUser(dummyUserData);
+    }
+
+        const fetchUserChats = async () => {
+
+            setChats(dummyChats);
+            setSelectedChat(dummyChats[0]);
+
+        }
+
+        useEffect(() => {
+            if (theme === 'dark') {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        }, [theme]);
+
+
+
+
+    useEffect(() => {
+        if (user) {
+            fetchUserChats();
+        }
+        else {
+            setChats([]);
+            setSelectedChat(null);
+        }
+       
+    }, [user]);
+
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+
+
+    const value = {
+
+        navigate, user, setUser, chats, setChats,
+        selectedChat, setSelectedChat, theme, setTheme,
+    }
 
     return (
         <AppContext.Provider value={value}>
