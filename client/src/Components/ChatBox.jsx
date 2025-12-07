ff40346350ef47076522e15663fd75a993ed70f6
Pasindu <chamipasindu2204@gmail.com>
2025-12-07 20:43:39 +0530

    msg.jsx building

--- client/src/Components/ChatBox.jsx
+++ client/src/Components/ChatBox.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import { useAppContext } from '../Context/AppContext.jsx';
-import { useSearchParams } from 'react-router-dom';
 import { assets } from '../assets/assets.js';
+import Message from './Message.jsx';
 
 const ChatBox = () => {
 
@@ -20,17 +20,22 @@ useEffect(() => {
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 
     xl:mx-30 max-md:mt-14 2xl:pr-40'>
 
-      {/* chat meassages area */}
+      {/* chat messages area */}
       <div className='flex-1 mb-5 overflow-y-scroll'>
          {messages.length === 0 && (
           <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
             <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark}
              alt='' className='w-full max-w-56 sm:max-w-68'/>
-             <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white'>ASk me Anything...</p>
+             <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white'>
+              ASk me Anything...</p>
           </div>
          )}
 
-         {messages.map((message, index) => )}
+         {messages.map((message, index) => 
+         <Message
+         key={index} 
+         message={message}
+         />)}
 
       </div>
 
--- client/src/Components/Message.jsx
+++ client/src/Components/Message.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { assets } from '../assets/assets.js';
 
-const Message = ({Message}) => {
+
+const Message = ({message}) => {
   return (
     <div>
-      {Message.role === 'user' ? 
+      {message.role === 'user' ? 
         (
           <div className='flex items-start justify-end my-4 gap-2'>
             <div className='flex flex-col gap-2 p-2 px-4
             bg-slate-50 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl'>
-              <p className='text-sm dark:text-primary'>{Message.content}</p>
-              <span className='text-xs text-gray-400 dark:text-[#B1A6C0]'>{Message.timestamp}</span>
+              <p className='text-sm dark:text-primary'>{message.content}</p>
+              <span className='text-xs text-gray-400 dark:text-[#B1A6C0]'>{message.timestamp}</span>
               </div>
               <img src={assets.user_icon} alt='user' className='w-8 rounded-full' />
           </div>
@@ -18,7 +20,19 @@ const Message = ({Message}) => {
         : 
           // Ai Answer
         (
-          <div></div>
+          <div className='inline-flex flex-col gap-2 p-2 px-4 max-w-2xl bg-primary/20
+          dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md my-4'>
+             {message.isImage ? (
+              <img src={message.content} alt='generated_image' className='w-full max-w-md mt-2 rounded-full'/>
+             )
+             :
+            (
+              <div className='text-sm dark:text-primary reset-tw'>
+                {message.content}
+              </div>
+            )}
+            <span>{message.timestamp}</span>
+          </div>
         )
       }
     </div>
@@ -26,3 +40,4 @@ const Message = ({Message}) => {
 }
 
 export default Message
+
