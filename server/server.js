994419ba7be01e1dfa87849f9a2a4a0ffc79e7f8
Pasindu <chamipasindu2204@gmail.com>
2025-12-09 00:26:57 +0530

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './configs/db.js';
+
+dotenv.config();
 
 const app = express();
+
+
+
+await connectDB();
 
 // middlewares
 app.use(cors());
