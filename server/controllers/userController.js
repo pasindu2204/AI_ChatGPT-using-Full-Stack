312f35f245010ae818bf13201f8a11fd0ee4b9df
Pasindu <chamipasindu2204@gmail.com>
2025-12-09 11:36:46 +0530

    user authantication. login api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-
+import bcrypt from "bcryptjs";
 
 // genarate token
 const generateToken = (id) => {
@@ -33,5 +33,40 @@ export const registerUser = async (req, res) => {
 
     } catch (error) {
         res.json({ success: false, message: 'Server Error' });
+    }
+}
+
+//api to login user
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+try {
+    const user = await User.findOne({ email });
+    if (user) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = generateToken(user._id);
+            return res.json({
+                success: true,
+                user,
+                token,
+            });
+        } else {
+            return res.json({ success: false, message: 'Invalid email or password' });
+        }
+    }
+} catch (error) {
+    return res.json({ success: false, message: 'Server Error' });
+}}
+
+
+// api to get user data 
+export const getUser = async (req, res) => {
+
+    try {
+        const user = req.user;
+        return res.json({ success: true, user });
+    } catch (error) {
+        return res.json({ success: false, message: 'Server Error' });
     }
 }
